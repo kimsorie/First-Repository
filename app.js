6419276f5f64fb59b0541e78ab3dc24000662784
Karlie Drinen <kdrinen@Karlies-MacBook-Pro-2.local>
2016-09-19 17:22:07 -0500

    init html/css/js/added images

---
+++ app.js
@@ -0,0 +1,28 @@
+$(function(){
+
+
+$('.b').click(function() {
+  $('.b').fadeOut(1000);
+});
+$( '.c').click(function() {
+  $( '.c' ).fadeOut(1000);
+});
+$('.d').click(function() {
+  $('.d').fadeOut(1000);
+});
+$('.e').click(function() {
+  $('.e').fadeOut(1000);
+});
+$('.f').click(function() {
+  $('.f').fadeOut(1000);
+});
+$('.g').click(function() {
+  $('.g').fadeOut(1000);
+});
+$('.h').click(function() {
+  $('.h').fadeOut(1000);
+});
+$('.i').click(function() {
+  $('.i').fadeOut(1000);
+});
+});
